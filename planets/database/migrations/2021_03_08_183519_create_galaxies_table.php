414c4fec44615da38bd435f0b2b1578265ca3bcf
Dagmar Svobodová <dagmarsvobodova@itadmins-MacBook-Pro.local>
2021-03-09 09:31:20 +0100

    Database structure. Migrations

---
+++ planets/database/migrations/2021_03_08_183519_create_galaxies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGalaxiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galaxies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->longText('info');
+            $table->unsignedBigInteger('subcategory_id')->default(4);
+            $table->unsignedBigInteger('univers_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galaxies');
+    }
+}
